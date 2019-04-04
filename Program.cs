e51977b067145130873d9e93c15686ba3ed7c1aa
Edward DeVries <ehdevries@gmail.com>
2019-04-04 13:32:48 -0400

    First fix: initialize non-nullable properties

--- Program.cs
+++ Program.cs
@@ -20,9 +20,9 @@ namespace NullableReferenceTypesDemo
 
     class Person
     {
-        public string FirstName { get; set; }
+        public string FirstName { get; set; } = string.Empty;
         public string MiddleName { get; set; }
-        public string LastName { get; set; }
+        public string LastName { get; set; } = string.Empty;
     }
 
     class PersonStore
