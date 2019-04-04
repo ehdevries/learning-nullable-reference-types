95fd8286180c3a769f6c8c825fe76d673df86885
Edward DeVries <ehdevries@gmail.com>
2019-04-04 13:14:05 -0400

    Add code that throws a NullReferenceException

--- Program.cs
+++ Program.cs
@@ -6,7 +6,28 @@ namespace NullableReferenceTypesDemo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Person p = PersonStore.GetFirstPerson();
+
+            int letterCount =
+                p.FirstName.Length +
+                p.MiddleName.Length +
+                p.LastName.Length;
+
+            Console.WriteLine($"Hello, {p.FirstName} {p.MiddleName} {p.LastName}");
+            Console.WriteLine($"You have {letterCount} letters in your name");
         }
+    }
+
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+    }
+
+    class PersonStore
+    {
+        public static Person GetFirstPerson()
+            => new Person { FirstName = "Pat", LastName = "McTest" };
     }
 }
